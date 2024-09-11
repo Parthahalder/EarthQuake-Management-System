c1bb30158a8f0194eea2afab1d3ff8cdf9f76752
Parthahalder <143213849+Parthahalder@users.noreply.github.com>
2024-09-11 21:00:33 +0530

    Update earthquake.py

--- earthquake.py
+++ earthquake.py
@@ -240,7 +240,7 @@ plt.show()
 features = earthquake_data[['latitude', 'longitude', 'depth', 'nst', 'gap', 'dmin', 'rms']]
 target = earthquake_data['mag']
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
-model = RandomForestRegressor(n_estimators=10, random_state=42)
+model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 # Predicting on test set
